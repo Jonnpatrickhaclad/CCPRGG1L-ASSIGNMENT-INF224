8fe5cc2ac23f54ce2e087077ddc57eba01c34d4c
Jonnpatrickhaclad <111546613+Jonnpatrickhaclad@users.noreply.github.com>
2022-10-21 10:22:23 +0800

    Create Assignment2.java

---
+++ Assignment2.java
@@ -0,0 +1,66 @@
+public class Assignment2 {
+    public class assignment2 {
+
+        public static void main(String[] args) throws Exception {
+    
+            int count = 0;
+    
+             while (count !=3){
+    
+                System.out.println("JONN PATRICK");
+    
+                count ++;
+    
+            }
+    
+            // do while loop
+    
+             int i = 0;
+    
+             do {
+    
+                System.out.println("BESAS");
+    
+                 i ++;
+    
+             } while (i != 4);
+    
+        
+    
+             //for loop
+    
+             for (int l = 0; l != 5;  l++) {
+    
+                System.out.println("HACLAD");
+    
+             }
+    
+             // char array
+    
+            
+    
+             char[] myFirstName = {'J','O','N','N','P','A','T','R','I','C','K'};
+    
+             for (int num = 0; num < myFirstName.length; num++) {
+    
+                System.out.print(myFirstName[num]);
+    
+             }
+    
+            
+    
+            System.out.println();
+    
+             // firstname reverse
+    
+             for (int num = myFirstName.length-1; num >= 0; num--) 
+    
+                System.out.print(myFirstName[num]);
+    
+            
+    
+        }
+    
+    }   
+    
+}
